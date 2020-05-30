9b3b54639ab3475fca09c2e3396c93e8730d86c8
Lucas Castilho <lucas.hco@hotmail.com>
2020-05-30 17:06:08 -0300

    Projeto finalizado

---
+++ main.js
@@ -0,0 +1,44 @@
+const masks = {
+  cpf (value) {
+    return value
+      .replace(/\D/g, '')
+      .replace(/(\d{3})(\d)/, '$1.$2')
+      .replace(/(\d{3})(\d)/, '$1.$2')
+      .replace(/(\d{3})(\d{1,2})/, '$1-$2')
+      .replace(/(-\d{2})\d+?$/, '$1')
+  },
+  cnpj (value) {
+    return value
+      .replace(/\D/g, '')
+      .replace(/(\d{2})(\d)/, '$1.$2')
+      .replace(/(\d{3})(\d)/, '$1.$2')
+      .replace(/(\d{3})(\d)/, '$1/$2')
+      .replace(/(\d{4})(\d)/, '$1-$2')
+      .replace(/(-\d{2})\d+?$/, '$1')
+  },
+  telefone (value) {
+    return value
+      .replace(/\D/g, '')
+      .replace(/(\d{2})(\d)/, '($1) $2')
+      .replace(/(\d{4})(\d)/, '$1-$2')
+      .replace(/(\d{4})-(\d)(\d{4})/, '$1$2-$3')
+      .replace(/(-\d{4})\d+?$/, '$1')
+  },
+  cep (value) {
+    return value
+      .replace(/\D/g, '')
+      .replace(/(\d{5})(\d)/, '$1-$2')
+      .replace(/(-\d{3})\d+?$/, '$1')
+  }
+};
+
+let inputArray = document.querySelectorAll('[data-simplemask]');
+
+inputArray.forEach(input => {
+  const maskType = input.dataset.simplemask;
+  
+  input.addEventListener('input', (event) => {
+    
+  	event.target.value = masks[maskType](event.target.value);
+  });
+});
